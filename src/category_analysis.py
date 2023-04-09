775e80727a51afda973742b099f014f6df444912
Peyman <peyman.xt@gmail.com>
2023-04-09 19:36:38 +0100

    Performs K-means clustering on the countries in category_analysis.py

--- src/category_analysis.py
+++ src/category_analysis.py
@@ -100,8 +100,88 @@ def get_sorted_list_of_countries_based_on_genre_excluding_none(genre):
 
 # the following line calls the function to get the sorted list of countries based on
 # the number of keywords belonging to the genre "Action/Fighting/Horror"
-get_sorted_list_of_countries_based_on_genre_including_none("Action/Fighting/Horror")
+# get_sorted_list_of_countries_based_on_genre_including_none("Action/Fighting/Horror")
 
 # the following line calls the function to get the sorted list of countries based on
 # the number of keywords belonging to the genre "Action/Fighting/Horror" excluding the none genre
-get_sorted_list_of_countries_based_on_genre_excluding_none("Action/Fighting/Horror")
+# get_sorted_list_of_countries_based_on_genre_excluding_none("Action/Fighting/Horror")
+
+
+
+#the following code assigns each country a point in space  based on the number of keywords belonging to these genres: "Action/Fighting/Horror", "Adventure" abd "RPG"
+def assign_countries_points_in_space(country_to_genre_dict, country_to_num_of_keywords_dict):
+    genre_list = ["Action/Fighting/Horror", "Adventure", "RPG", "Simulation", "Strategy", "Sports", "Puzzle", "Platformer", "Casual", "Family", "None"]
+    # initialize a dictionary to store the points of each country
+    country_to_points_dict = {}
+    # loop through all the countries
+    for country_name in country_to_genre_dict:
+        # initialize a list to store the points of the country
+        points_list = []
+        # loop through all the genres
+        for genre in genre_list:
+            # check if the genre is in the country
+            if country_to_genre_dict[country_name].get(genre) is None:
+                genre_count = 0
+            else:
+                genre_count = country_to_genre_dict[country_name][genre]
+
+            # get the number of keywords in the country
+            num_of_keywords = country_to_num_of_keywords_dict[country_name]
+            # compute the percentage of keywords belonging to the genre in the country
+            percentage = genre_count / num_of_keywords * 100
+            # add the percentage to the list
+            points_list.append(percentage)
+        # save the list of points in the dictionary
+        country_to_points_dict[country_name] = points_list
+    return country_to_points_dict
+
+
+# the following line calls the function to assign each country a point in space
+country_to_genre_dict, country_to_num_of_keywords_dict = compute_each_country_genre_count(country_trends_dict, country_names, keyword_to_genre_dictionary)
+country_to_points_dict = assign_countries_points_in_space(country_to_genre_dict, country_to_num_of_keywords_dict)
+print(country_to_genre_dict["United Kingdom"])
+print(country_to_points_dict["United Kingdom"])
+print(country_to_points_dict["Canada"])
+print(country_to_points_dict["Australia"])
+print(country_to_points_dict["Iran"])
+
+
+from sklearn.cluster import KMeans
+
+# the following code performs the k-means clustering algorithm
+def perform_k_means_clustering(country_to_points_dict, num_of_clusters):
+    # initialize a list to store the points of each country
+    points_list = []
+    # loop through all the countries
+    for country_name in country_to_points_dict:
+        # get the points of the country
+        points = country_to_points_dict[country_name]
+        # add the points to the list
+        points_list.append(points)
+    # initialize the k-means clustering object
+    kmeans = KMeans(n_clusters=num_of_clusters, random_state=0).fit(points_list)
+    # get the labels of the countries
+    labels = kmeans.labels_
+    # initialize a dictionary to store the countries in each cluster
+    cluster_to_countries_dict = {}
+    # loop through all the countries
+    for i in range(len(country_to_points_dict)):
+        # get the country name
+        country_name = list(country_to_points_dict.keys())[i]
+        # get the cluster of the country
+        cluster = labels[i]
+        # check if the cluster is in the dictionary
+        if cluster_to_countries_dict.get(cluster) is None:
+            # add the country to the dictionary
+            cluster_to_countries_dict[cluster] = [country_name]
+        else:
+            # add the country to the dictionary
+            cluster_to_countries_dict[cluster].append(country_name)
+    return cluster_to_countries_dict
+
+# the following line calls the function to perform the k-means clustering algorithm
+cluster_to_countries_dict = perform_k_means_clustering(country_to_points_dict, 5)
+print(cluster_to_countries_dict[0])
+print(cluster_to_countries_dict[1])
+print(cluster_to_countries_dict[3])
+
